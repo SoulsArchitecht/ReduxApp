ffa4a79249befd5ac2470fb5a825382b26890ac8
Serge <sshibkojavadev@gmail.com>
2023-12-22 17:18:34 +0800

    3) added react

--- src/index.js
+++ src/index.js
@@ -1,52 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {createStore, bindActionCreators} from 'redux';
+import {createStore} from 'redux';
 import './index.css';
-import reducer from './reducer'
-//import {inc, dec, rnd} from './actions'
-import * as actions from './actions';
-
+import reducer from './reducer';
+import App from './components/App';
+import {Provider} from 'react-redux';
 
 const store = createStore(reducer);
-
-const {dispatch, subscribe, getState} = store;
-
-
-const update = () => {
-  document.getElementById('counter').textContent = getState().value;
-}
-
-subscribe(update);
-
-// const bindActionCreator = (creator, dispatch) => (...args) => {
-//   dispatch(creator(...args))
-// }
-
-// const incDispatch = bindActionCreators(inc, dispatch);
-// const decDispatch = bindActionCreators(dec, dispatch);
-// const rndDispatch = bindActionCreators(rnd, dispatch);
-
-// const {incDispatch, decDispatch, rndDispatch} = bindActionCreators({
-//     incDispatch: inc,
-//     decDispatch: dec,
-//     rndDispatch: rnd
-// }, dispatch)
-
-const {incDispatch, decDispatch, rndDispatch} = bindActionCreators(actions, dispatch);
-
-document.getElementById('inc').addEventListener('click', incDispatch)
-
-document.getElementById('dec').addEventListener('click', decDispatch);
-
-document.getElementById('rnd').addEventListener('click', () => {
-  const value = Math.floor(Math.random() * 10);
-  rndDispatch(value);
-})
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <>
-    </>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>
 );
+
+
+
+
+
+
---
+++ src/indexOld2.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {createStore, bindActionCreators} from 'redux';
+import './index.css';
+import reducer from './reducer'
+import Counter from './Counter';
+import * as actions from './actions';
+
+
+const store = createStore(reducer);
+
+const {dispatch, subscribe, getState} = store;
+
+const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
+
+const update = () => {
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(
+      <React.StrictMode>
+        <Counter
+            counter={getState().value()}
+            inc={inc}
+            dec={dec}
+            rnd={() => {
+                const value = Math.floor(Math.random() * 10);
+                rnd(value);    
+            }}
+        />
+      </React.StrictMode>
+    );
+}
+
+update();
+subscribe(update);
+
+
+
+
